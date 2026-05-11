c8f8d983af68d512443c545904b90b44d9a258d0
chimakes <chimakes3d@gmail.com>
2026-05-11 18:28:13 -0400

    add github link and go live with vercel

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Cute Cow — Procedural Cow Spots (Simplex Noise)
 
+A cute cow with procedural spots generated using simplex noise
+
+## Demo
+https://cow-simplex-noise.vercel.app/
 
 ![Screenshot](screenshot.png)
--- src/index.jsx
+++ src/index.jsx
@@ -9,11 +9,18 @@ import { cameraPosition } from 'three/tsl'
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        camera={{
-            position: [1.1, 1.2, 4.5]
-        }}
-    >   
-        <Experience />
-    </Canvas>
+    <>
+        <Canvas
+            camera={{
+                position: [1.1, 1.2, 4.5]
+            }}
+        >   
+            <Experience />
+        </Canvas>
+        <div className="github-logo">
+            <a href="https://github.com/chimakes/cow_simplex_noise" target="_blank">
+                <img src="/assets/GitHub_Invertocat_White.svg" alt="github logo" width="32" height="32" />
+            </a>
+        </div>
+    </>
 )
